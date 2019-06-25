a76850d79257db41b139fa330da2c7c273b6e457
Observer KRypt0n_ <suimin.tu.mu.ga.mi@gmail.com>
2019-06-25 16:59:28 +0200

    VoidFramework 3.1.0

---
+++ README.md
@@ -0,0 +1,29 @@
+# VoidFramework
+
+**VoidFramework** - инструмент для создания графических приложений для **Windows** на базе **.NET Framework** и **PHP**
+
+Основной проект: [WinForms PHP](https://github.com/KRypt0nn/WinForms-PHP)
+
+## Установка (Qero)
+
+```cmd
+php Qero.phar install KRypt0nn/VoidFramework
+```
+
+> Qero: [тык](https://github.com/KRypt0nn/Qero)
+
+## Использование
+
+После установки создастся папка **app** рядом с папкой **qero-packages**. В ней размещается само приложение **VoidFramework**. В качестве точки входа используется файл **start.php**
+
+Для запуска приложения вы можете прописать команду
+
+```cmd
+php Qero.phar start
+```
+
+запустить файл **start.bat** или создать какой-нибудь ярлык. Это не суть важно. Запуск приложения происходит через файл **%VoidFramework%/core/WinForms PHP.exe** с аргументом в виде пути к файлу точки входа *(подробнее в **start.bat**)*
+
+Подробности функционала **VoidEngine** смотреть в основном проекте *(ссылка выше)*
+
+Автор: [Подвирный Никита](https://vk.com/technomindlp). Специально для [Enfesto Studio Group](https://vk.com/hphp_convertation)
--- after_install.php
+++ after_install.php
@@ -7,6 +7,10 @@ $info['scripts']['start'] = '"qero-packages'. str_replace (dirname (__DIR__, 3),
 
 file_put_contents (QERO_DIR .'/qero-info.json', json_encode ($info, defined ('JSON_PRETTY_PRINT') ? JSON_PRETTY_PRINT : 0));
 
+if (!file_exists (QERO_DIR .'/start.bat'))
+    file_put_contents (QERO_DIR .'/start.bat', '@echo off
+'. $info['scripts']['start']);
+
 if (!is_dir (QERO_DIR .'/app'))
     mkdir (QERO_DIR .'/app');
 
@@ -15,7 +19,37 @@ if (!file_exists (QERO_DIR .'/app/start.php'))
     
 namespace VoidEngine;
 
+const APP_DIR = __DIR__;
+chdir (APP_DIR);
+
 require __DIR__ .\'/../qero-packages/autoload.php\';
 
-pre (\'Hello, World!\');
+$parser = new VLFParser (__DIR__. \'/app.vlf\', [
+    \'strong_line_parser\'            => false,
+    \'ignore_postobject_info\'        => true,
+    \'ignore_unexpected_method_args\' => true,
+
+    \'use_caching\' => true,
+    \'debug_mode\'  => false
+]);
+
+$objects = VLFInterpreter::run ($parser);
+
+$APPLICATION->run ($objects[\'MainForm\']);
+');
+
+if (!file_exists (QERO_DIR .'/app/app.vlf'))
+    file_put_contents (QERO_DIR .'/app/app.vlf', 'Form MainForm
+    caption: \'Hello, World!\'
+    size: [400, 300]
+    startPosition: fspCenterScreen
+
+    Button MainButton
+    bounds: [8, 8, 120, 32]
+    caption: \'Click Me!\'
+
+    ClickEvent:^ function ($self)
+        {
+            pre ($self);
+        }
 ');
