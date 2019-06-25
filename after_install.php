30880a1a3f30c6c407252fdb425aa52cd5a995b6
Observer KRypt0n_ <suimin.tu.mu.ga.mi@gmail.com>
2019-06-25 16:34:31 +0200

    VoidFramework 3.1.0

--- after_install.php
+++ after_install.php
@@ -3,7 +3,7 @@
 $info = file_exists (QERO_DIR .'/qero-info.json') ?
     json_decode (file_get_contents (QERO_DIR .'/qero-info.json'), true) : array ();
 
-$info['scripts']['start'] = '"'. str_replace (dirname (__DIR__, 3), '', __DIR__) .'/core/WinForms PHP.exe" "app/start.php"';
+$info['scripts']['start'] = '"qero-packages'. str_replace (dirname (__DIR__, 3), '', __DIR__) .'/core/WinForms PHP.exe" "app/start.php"';
 
 file_put_contents (QERO_DIR .'/qero-info.json', json_encode ($info, defined ('JSON_PRETTY_PRINT') ? JSON_PRETTY_PRINT : 0));
 
