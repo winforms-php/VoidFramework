fc4acab21caf59083bf1705f5aff2b5795b633c2
Observer KRypt0n_ <suimin.tu.mu.ga.mi@gmail.com>
2019-07-18 13:25:57 +0200

    VoidFramework 3.2.0 hotfix - исправлен файл "after_install.php"

--- after_install.php
+++ after_install.php
@@ -20,26 +20,16 @@ function dir_copy (string $from, string $to): bool
 $info = file_exists (QERO_DIR .'/qero-info.json') ?
     json_decode (file_get_contents (QERO_DIR .'/qero-info.json'), true) : array ();
 
-$info['scripts']['start'] = '"qero-packages'. str_replace (dirname (__DIR__, 3), '', __DIR__) .'/core/WinForms PHP.exe" "app/start.php"';
+$info['scripts']['start'] = '"qero-packages'. str_replace (dirname (__DIR__, 3), '', __DIR__) .'/core/VoidCore.exe" "app/start.php"';
 
 file_put_contents (QERO_DIR .'/qero-info.json', json_encode ($info, defined ('JSON_PRETTY_PRINT') ? JSON_PRETTY_PRINT : 0));
-
-if (!file_exists (QERO_DIR .'/start.bat'))
-    file_put_contents (QERO_DIR .'/start.bat', '@echo off
+file_put_contents (QERO_DIR .'/start.bat', '@echo off
 '. $info['scripts']['start']);
 
 if (!is_dir (QERO_DIR .'/app'))
 {
     mkdir (QERO_DIR .'/app');
-    
     dir_copy (__DIR__ .'/app_bundle', QERO_DIR .'/app');
-
-    // Обновление переменных
-    file_put_contents (QERO_DIR .'/app/start.php', str_replace ([
-        '%FRAMEWORK_PATH%'
-    ], [
-        'qero-packages'. str_replace (dirname (__DIR__, 3), '', __DIR__)
-    ], file_get_contents (QERO_DIR .'/app/start.php')));
 }
 
 Qero\dir_delete (__DIR__ .'/app_bundle');
